5a0c2865f7c61f23eadac7e040611ab70827ba90
2hats <info@2hats.com.au>
2022-06-03 11:42:26 +1000

    updated moment.js dependency

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # bootstrap-4-calendar
-A nicely designed bootstrap 4 calendar template
+A nicely designed bootstrap 4 calendar template.
 
-Run `npm install` to install the moment.js package.
+Use `Live Server` to run the demo project locally. 
 
-Use `Live Server` to run the project. 
+Or visit the sandbox https://88lvfm.csb.app/ online.
 
 ![screenshot](https://github.com/ylli2000/bootstrap-4-calendar/blob/master/screenshot.PNG "screenshot")
--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
-import {mockData} from './mockData.js';
-import {Spinner} from './spinner.js';
-import {Calendar} from './calendar.js';
+import { mockData } from './mockData.js';
+import { Spinner } from './spinner.js';
+import { Calendar } from './calendar.js';
 
+const ready = callback => {
+	if (document.readyState !== 'loading') callback();
+	else if (document.addEventListener)
+		document.addEventListener('DOMContentLoaded', callback);
+	else
+		document.attachEvent('onreadystatechange', function () {
+			if (document.readyState === 'complete') callback();
+		});
+};
 
-document.addEventListener("DOMContentLoaded", async ()=>{
-    const cal = Calendar('calendar');
-    const spr = Spinner('calendar'); 
-    await spr.renderSpinner().delay(0);
-    cal.bindData(mockData);
-    cal.render();
+ready(async () => {
+	const cal = Calendar('calendar');
+	const spr = Spinner('calendar');
+	await spr.renderSpinner().delay(0);
+	cal.bindData(mockData);
+	cal.render();
 });
